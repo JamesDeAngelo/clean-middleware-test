8bc50a7ed298f33f25647c9057847c7566edde62
James DeAngelo <Jamesdeangelo1991@gmail.com>
2025-11-27 16:40:44 -0500

    Complete AI voice agent with audio streaming

--- config/lawyers.js
+++ config/lawyers.js
@@ -1,5 +1,4 @@
 const LAWYER_CONFIGS = {
-  // Use your FROM_NUMBER from .env
   [process.env.FROM_NUMBER]: {
     airtableKey: process.env.AIRTABLE_API_KEY,
     baseId: process.env.AIRTABLE_BASE_ID,
@@ -11,15 +10,6 @@ const LAWYER_CONFIGS = {
     ],
     includeTranscript: false
   }
-  
-  // To add more lawyers, add more entries like:
-  // "+15551234567": {
-  //   airtableKey: "keyXXXXX",
-  //   baseId: "appYYYYY",
-  //   table: "Leads",
-  //   questionFlow: ["Name?", "Issue?"],
-  //   includeTranscript: false
-  // }
 };
 
 module.exports = LAWYER_CONFIGS;
