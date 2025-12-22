a3709f1bc6a2e8882dd976dfaa06fac9645cb049
James DeAngelo <Jamesdeangelo1991@gmail.com>
2025-12-22 12:59:28 -0500

    Overwrite with local clean version

--- airtable.js
+++ airtable.js
@@ -99,6 +99,8 @@ module.exports = {
   saveLeadToAirtable,
   testAirtableConnection
 };
-                                                                                                                  
+
+
+                                                                                                                
 
 
